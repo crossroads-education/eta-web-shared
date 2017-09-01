80f761381e90f1f13a2c041ba4df09b10f745fca
Alex Hicks <aldahick@iupui.edu>
2017-09-01 08:28:18 -0400

    Port scroll-head implementation

---
+++ static/js/shared/scroll-head.ts
@@ -0,0 +1,33 @@
+interface JQuery {
+    /**
+     * Creates a sticky header for table `this`
+     * Table header is cloned and given fixed position
+     */
+    scrollHead(): JQuery;
+}
+
+(function($: JQueryStatic) {
+    $.fn.scrollHead = function(this: JQuery): any {
+        return $.each(this, () => {
+            const tableOffset: number = this.offset().top;
+            const head: JQuery = this.find("thead").clone();
+            const fixedHead: JQuery = $("<table>").append(head);
+            fixedHead.prop("id", "fixed-head");
+            fixedHead.addClass("col-xs-12 schedule-container");
+            const fixedHeadContainer: JQuery = $("<div>").append(fixedHead);
+            fixedHeadContainer.prop("id", "fixed-head-container");
+            fixedHeadContainer.addClass("row");
+            $("#body").append(fixedHeadContainer);
+            $(document.body).trigger("scrollHead.loaded");
+            $(window).on("scroll", () => {
+                const $window: JQuery = $(window);
+                const offset: number = $window.scrollTop();
+                if (offset >= tableOffset && fixedHeadContainer.is(":hidden")) {
+                    fixedHeadContainer.show();
+                } else if (offset < tableOffset) {
+                    fixedHeadContainer.hide();
+                }
+            });
+        });
+    };
+})(jQuery);
