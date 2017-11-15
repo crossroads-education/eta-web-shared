f3046022321b257e957c51da5d60655a2c49ae87
Alex Hicks <aldahick@iupui.edu>
2017-11-15 15:40:23 -0500

    Fix deprecated helper usage

--- transformers/CssTransformer.ts
+++ transformers/CssTransformer.ts
@@ -11,7 +11,7 @@ export default class CssTransformer extends eta.IRequestTransformer {
         }
         this.redirects = {};
         Object.keys(eta.config.modules).forEach(k => {
-            this.redirects = eta.object.merge(eta.config.modules[k].css, this.redirects);
+            this.redirects = eta._.defaults(eta.config.modules[k].css, this.redirects);
         });
     }
 
