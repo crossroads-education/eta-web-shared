798568e32c4d75f0efb27c2933b17862527a5f80
Alex Hicks <alex@alexhicks.net>
2018-03-01 07:42:14 -0500

    Remove process-based test running

--- lifecycle/CLIProcessLifecycle.ts
+++ lifecycle/CLIProcessLifecycle.ts
@@ -2,7 +2,6 @@ import * as eta from "../eta";
 import * as db from "../db";
 import Application from "../../../server/Application";
 import SeedController from "../controllers/cre/v1/db";
-import TestsController from "../controllers/cre/v1/tests";
 
 export default class CLIProcessLifecycle extends eta.ILifecycleHandler {
     public register(app: Application): void {
@@ -16,8 +15,6 @@ export default class CLIProcessLifecycle extends eta.ILifecycleHandler {
                 let promise: Promise<void>;
                 if (msg.endsWith(":seed")) {
                     promise = new SeedController(controllerPartial).seed();
-                } else if (msg.endsWith(":test")) {
-                    promise = new TestsController(controllerPartial).run();
                 }
                 promise.then(() => {
                     process.send(msg);
