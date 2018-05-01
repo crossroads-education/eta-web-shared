f196e2c55b679b96254e68ad79387411fc55d27d
Erik Weitnauer <eweitnauer@gmail.com>
2018-05-01 15:28:33 -0400

    small change to permission transformer and added test case for it

--- lib/PermissionsTransformer.ts
+++ lib/PermissionsTransformer.ts
@@ -2,10 +2,9 @@ import * as eta from "@eta/eta";
 
 const PermissionsTransformer = (prefix: string): ClassDecorator => {
     return (target: any) => {
-        eta.object.recursiveKeys(target).forEach(keys => {
-            const item = keys.slice(0, -1).reduce((p, v) => p[v], target);
-            if (typeof(item[keys[keys.length - 1]]) !== "string") return;
-            item[keys[keys.length - 1]] = prefix + keys.join("/");
+        eta.object.forEachPath(target, (path: string[], obj: any, key: string) => {
+          if (typeof(obj[key]) !== "string") return;
+          obj[key] = prefix + path.join("/");
         });
     };
 };
---
+++ test/lib/PermissionsTransformer.test.ts
@@ -0,0 +1,21 @@
+import { expect } from "chai";
+import PermissionsTransformer from "../../lib/PermissionsTransformer";
+
+describe.only("PermissionsTransformer", () => {
+  @PermissionsTransformer("Element/")
+  class ElementPermission {
+      static Analytics = {
+            Query: {
+                Advisor: "",
+                Coach: "",
+                Director: ""
+            }
+        };
+    }
+
+    it("should transform the permissions correctly", () => {
+      expect(ElementPermission.Analytics.Query.Advisor).to.equal("Element/Analytics/Query/Advisor");
+      expect(ElementPermission.Analytics.Query.Coach).to.equal("Element/Analytics/Query/Coach");
+      expect(ElementPermission.Analytics.Query.Director).to.equal("Element/Analytics/Query/Director");
+    });
+});
