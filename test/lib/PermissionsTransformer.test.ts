4c89c8829b62013fec1d10c9f70832b7bac8444e
Erik Weitnauer <eweitnauer@gmail.com>
2018-05-03 14:08:29 -0400

    indent 4 spaces

--- test/lib/PermissionsTransformer.test.ts
+++ test/lib/PermissionsTransformer.test.ts
@@ -2,9 +2,9 @@ import { expect } from "chai";
 import PermissionsTransformer from "../../lib/PermissionsTransformer";
 
 describe.only("PermissionsTransformer", () => {
-  @PermissionsTransformer("Element/")
-  class ElementPermission {
-      static Analytics = {
+    @PermissionsTransformer("Element/")
+    class ElementPermission {
+        static Analytics = {
             Query: {
                 Advisor: "",
                 Coach: "",
@@ -14,8 +14,8 @@ describe.only("PermissionsTransformer", () => {
     }
 
     it("should transform the permissions correctly", () => {
-      expect(ElementPermission.Analytics.Query.Advisor).to.equal("Element/Analytics/Query/Advisor");
-      expect(ElementPermission.Analytics.Query.Coach).to.equal("Element/Analytics/Query/Coach");
-      expect(ElementPermission.Analytics.Query.Director).to.equal("Element/Analytics/Query/Director");
+        expect(ElementPermission.Analytics.Query.Advisor).to.equal("Element/Analytics/Query/Advisor");
+        expect(ElementPermission.Analytics.Query.Coach).to.equal("Element/Analytics/Query/Coach");
+        expect(ElementPermission.Analytics.Query.Director).to.equal("Element/Analytics/Query/Director");
     });
 });
