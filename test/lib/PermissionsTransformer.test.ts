ff758cade0de54a5433e194a0b3f51ee10250287
Erik Weitnauer <eweitnauer@gmail.com>
2018-11-29 14:07:23 -0500

    enable all tests

--- test/lib/PermissionsTransformer.test.ts
+++ test/lib/PermissionsTransformer.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import PermissionsTransformer from "../../lib/PermissionsTransformer";
 
-describe.only("PermissionsTransformer", () => {
+describe("PermissionsTransformer", () => {
     @PermissionsTransformer("Element/")
     class ElementPermission {
         static Analytics = {
